3971a4a4a6c4a2482c4cb8e2d9ecdf4c15dd06c9
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2024-09-14 14:05:38 -0300

    Criando os templates necessários para o layout

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -5,3 +5,15 @@ def index(request):
         request,
         'pages/index.html',
     )
+
+def page(request):
+    return render(
+        request,
+        'pages/page.html',
+    )
+
+def post(request):
+    return render(
+        request,
+        'pages/post.html',
+    )
