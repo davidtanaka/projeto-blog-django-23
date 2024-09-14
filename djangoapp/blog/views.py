76e41fcebc4cb889e268e7b210c883f25a9e898c
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2024-09-14 15:19:07 -0300

    Layout do blog completamente pronto.

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,19 +1,46 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
+posts = list(range(1000))
+
+
 def index(request):
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     return render(
         request,
         'pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
     )
 
+
 def page(request):
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     return render(
         request,
         'pages/page.html',
+        {
+            # 'page_obj': page_obj,
+        }
     )
 
+
 def post(request):
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     return render(
         request,
         'pages/post.html',
+        {
+            # 'page_obj': page_obj,
+        }
     )
