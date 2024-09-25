61e961f1f69a6057e38606ed9d297a8d0c0a88a6
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2024-09-24 21:01:40 -0300

    Criando o model Post, related_name em ForeignKey e outros detalhes

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -81,14 +81,16 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', 'change-me'),
-        'NAME': os.getenv('POSTGRES_DB', 'change-me'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
         'USER': os.getenv('POSTGRES_USER', 'change-me'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'change-me'),
         'HOST': os.getenv('POSTGRES_HOST', 'change-me'),
         'PORT': os.getenv('POSTGRES_PORT', 'change-me'),
     }
 }
+
+
 
 
 # Password validation
