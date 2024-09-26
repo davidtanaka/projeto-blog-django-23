8bc67206ba37d65748f17af0cc8d63ae4c96e523
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2024-09-26 17:53:17 -0300

    Corrigindo erros, e fazendo o código funcionar

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -65,7 +65,7 @@ class Post(models.Model):
         unique=True, default="",
         null=False, blank=True, max_length=255
     )
-    excerpt = models.CharField(max_length=150)
+    excerpt = models.TextField(blank=True, null=True) 
     is_published = models.BooleanField(
         default=False,
         help_text=(
